02e2f8eeab22288390a4f015ceb09df826fa5582
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2020-03-26 09:54:15 +0100

    - FrameWork:   Add event in the 'main_input' framepanel - creteID, ratios, table2cfg:   Discard the rows with the first empty column in the task-table

--- src/preSanXoT/ratios.py
+++ src/preSanXoT/ratios.py
@@ -36,12 +36,19 @@ def read_command_table(ifiles):
     '''
     indata = dict()
     idta = ''
+    # read the multiple input files to string and split by command
     for f in ifiles.split(";"):
         with open(f, "r") as file:
             idta += file.read()
+    # create a list of tuples (command, dataframe with parameters)
     match = re.findall(r'\s*#([^\s]*)\s*([^#]*)', idta, re.I | re.M)
-    idta = [(c,pd.read_csv(io.StringIO(l), sep='\t', dtype=str, skip_blank_lines=True).dropna(how="all").dropna(how="all", axis=1).astype('str')) for c,l in match]    
+    idta = [(c,pd.read_csv(io.StringIO(l), sep='\t', dtype=str, skip_blank_lines=True).dropna(how="all").dropna(how="all", axis=1).astype('str')) for c,l in match]
+    # create a dictionary with the concatenation of dataframes for each command
     for c, d in idta:
+        # discard the rows when the first empty columns
+        if not d.empty:
+            l = list(d[d.iloc[:,0] == 'nan'].index)
+            d = d.drop(l)
         if c in indata:
             indata[c] = pd.concat( [indata[c], d], sort=False)
         else:
