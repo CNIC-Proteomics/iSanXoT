e553a8b4c6ca725f342e57def0b8b3d99cd262c9
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2021-03-20 16:36:22 +0100

    Minor changes

--- install/src/installer.py
+++ install/src/installer.py
@@ -228,13 +228,17 @@ def install_report(trep, req_new, req_loc):
             # check if the new package manager is already installed
             if (not trep in req_loc) or (not manager in req_loc[trep]):
                 if trep == 'EXEC':
+                    print("-- install executors")
                     man_dir = man[1] if len(man) > 1 else manager # get the name of output dir. By default, the given file
                     iok = install_exec_manager(manager, f"{exec_url}/{manager}", f"{execdir}/{man_dir}")
                 if trep == 'MANAGER':
+                    print("-- install packages")
                     iok = install_pkg_manager(manager)
                 elif trep == 'DATABASES':
+                    print("-- install databases")
                     iok = download_databases(manager)
                 elif trep == 'SAMPLES':
+                    print("-- install samples")
                     iok = download_samples(manager)
                 # save modules in the req local
                 if iok:
@@ -274,19 +278,15 @@ def main():
 
 
     # look through the new requirements for the excutor ---
-    print("-- install executors")
     req_loc = install_report('EXEC', req_new, req_loc)
 
     # look through the new requirements for the packages ---
-    print("-- install packages")
     req_loc = install_report('MANAGER', req_new, req_loc)
             
     # look through the new requirements for the databases ---
-    print("-- install databases")
     req_loc = install_report('DATABASES', req_new, req_loc)
     
     # look through the new requirements for the samples ---
-    print("-- install samples")
     req_loc = install_report('SAMPLES', req_new, req_loc)
         
     # write string with the new requiremens into local file ---
