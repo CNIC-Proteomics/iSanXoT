d237ddfd655b96901bf28163e397226636b36910
Jose Rodriguez <jmrodriguezc1977@gmail.com>
2021-04-24 00:43:33 +0200

    bug fixed

--- src/preSanXoT/createRels.py
+++ src/preSanXoT/createRels.py
@@ -203,6 +203,7 @@ def main(args):
     # HARD-CODE: Filter the GO terms based on the evidence codes:
     # http://geneontology.org/docs/guide-go-evidence-codes/
     # filters = "cat_GO_*:EXP,IDA,IPI,IMP,IGI,IEP,HTP,HDA,HMP,HGI,HEP"
+    filters = None
     
     logging.info("read input files of inferior header")
     l = []
